2a99e20f220cc991dea53dd6fbd01d815cad919b
mazax1ct <vova@giveandget.ru>
2020-12-20 17:06:15 +0300

    combobox

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,27 @@
 $(document).on('click', '.js-menu-toggler', function () {
   $('body').toggleClass('overflow');
-  $(this).toggleClass('is-active');
-  if($(this).hasClass('is-active')){
+  $('.header').toggleClass('menu-open');
+  if($('.header').hasClass('menu-open')){
     $(this).find('use').attr('xlink:href','#close');
   }else{
     $(this).find('use').attr('xlink:href','#burger');
   }
-  $('.header').toggleClass('menu-open');
+  return false;
+});
+
+function closeCombobox(evt) {
+  if (!$('.combobox__dropdown').is(evt.target) && $('.combobox__dropdown').has(evt.target).length === 0) {
+    $('.combobox').removeClass('is-open');
+    document.removeEventListener('click', closeCombobox);
+	}
+}
+
+$(document).on('click', '.combobox__button', function () {
+  $(this).parent().toggleClass('is-open');
+  if($(this).parent().hasClass('is-open')) {
+    document.addEventListener('click', closeCombobox);
+  } else {
+    document.removeEventListener('click', closeCombobox);
+  }
   return false;
 });
